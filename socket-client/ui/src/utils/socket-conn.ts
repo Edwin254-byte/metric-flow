1ed6678efe79391b256e01797d351021c32431ca
edu <edu@v3.lcl>
2024-03-17 22:35:45 +0300

    added socket.io dependency, updated the script and Docker file

--- socket-client/ui/src/utils/socket-conn.ts
+++ socket-client/ui/src/utils/socket-conn.ts
@@ -4,9 +4,10 @@ import { ClientSocket } from "./type";
 // TODO: add authentication using JWT
 // For now, Just comment and uncomment whenever you switch between production and local
 const lclUrl = "http://localhost:3300";
-const prdUrl = "http://socket-server.v3.lcl";
+const prdUrl = "http://192.168.10.4:3300";
+//const prdUrl = "http://socket-server.v3.lcl";
 
-const socket: ClientSocket = io(lclUrl, { auth: { token: "23jrtiheriufyqwidsf" } }); //our server is at :3000
+const socket: ClientSocket = io(prdUrl, { auth: { token: "23jrtiheriufyqwidsf" } }); //our server is at :3000
 
 // socket.on("connect", data => {
 //   console.log(data);
